6754ed62e5cbab50248992aa255d316bdf9c095a
zinyamonenaing <yamone789@gmail.com>
2021-11-22 11:16:06 +0630

    first commit

---
+++ record/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Record(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.title 
